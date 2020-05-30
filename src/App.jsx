d762355651be49623d7f07cd9f07baac63ab4b50
Ricardo Del Rio <rdelrio.ge@gmail.com>
2020-05-30 02:18:02 -0500

    Capacitacion OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.scss";
+import WorkOrder from "./components/WorkOrder";
+import Capacitacion from "./components/Capacitacion";
 
 function App() {
   return (
     <div className="App">
-      <h1>Rock!</h1>
+      <WorkOrder />
+      <div className="saltodepag"></div>
+      <Capacitacion />
     </div>
   );
 }
---
+++ src/components/Capacitacion.jsx
@@ -0,0 +1,244 @@
+import React from "react";
+import "./capacitacion.scss";
+
+function Capacitacion() {
+  return (
+    <div className="capacitacion">
+      <div className="rows">
+        <div className="row1">
+          <div className="logo"></div>
+          <div className="datosge">
+            <p>
+              <b>GE Healthcare</b>
+            </p>
+            <p>GE SISTEMAS MEDICOS DE MEXICO S.A. DE C.V.</p>
+            <p>Antonio Dovali Jaime No. 70, Torre B Piso 5</p>
+            <p> Col. Santa Fe Del. Alvaro Obregón, C.P. 01210 CDMX</p>
+            <p>Atención a Clientes Servicio 01 800 904 3400</p>
+          </div>
+        </div>
+        <h2>CONSTANCIA DE CAPACITACIÓN TÉCNICA Y OPERATIVA</h2>
+        <div className="row2">
+          <div className="datoscliente">
+            <div>
+              <span>Razón Social:</span>
+              <span>HGSZ/MF No. 3</span>
+            </div>
+            <div>
+              <span>Ciudad / Localidad: </span>
+              <span>CHILPANCINGO</span>
+            </div>
+            <div>
+              <span>Delegación:</span>
+              <span>GUERRERO</span>
+            </div>
+            <div>
+              <span>Dependencia:</span>
+              <span>IMSS</span>
+            </div>
+            <div>
+              <span>Dirección:</span>
+              <span></span>
+            </div>
+            <div className="telcliente">
+              <span>Teléfono:</span>
+              <span></span>
+            </div>
+          </div>
+          <div className="datosequipo">
+            <div className="sid">
+              <span>SID:</span>
+              <b>57618</b>
+            </div>
+            <div className="equipo">
+              <span>Equipo:</span>
+              <span>Ventilador de traslado pediátrico-adulto</span>
+            </div>
+            <div>
+              <span>Modelo:</span>
+              <span>Falcon 202 Evo</span>
+            </div>
+            <div>
+              <span>Serie:</span>
+              <span>57618</span>
+            </div>
+            <div className="modalidad">
+              <span>Modalidad:</span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+        <div className="row3">
+          <div className="row3L">
+            <span className="fechadecapacitacion">Fecha de Capacitación</span>
+            <div className="titulosTiempos">
+              <span>Día</span>
+              <span>Mes</span>
+              <span>Año</span>
+            </div>
+            <div className="row3L-r">
+              <span>16</span>
+              <span>02</span>
+              <span>20</span>
+            </div>
+          </div>
+          <div className="row3R">
+            <b className="rowtitle"></b>
+            <div className="row3R-r">
+              <span>Tipo de contrato:</span>
+              <b>Contrato</b>
+            </div>
+            <div className="row3R-r">
+              <span>Contrato No:</span>
+              <b>19BI0127</b>
+            </div>
+          </div>
+        </div>
+        <div className="row4">
+          <h3 className="refsTitle">Lista de Asistencia </h3>
+          <div className="refsTitle row4-6c">
+            <b>Nombre completo</b>
+            <b>Matrícula</b>
+            <b>Área / Servicio</b>
+            <b>Cargo / Puesto</b>
+            <b>Turno</b>
+            <b>Puesto</b>
+          </div>
+          <div className="row4-6c">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="row4-6c">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="row4-6c">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="row4-6c">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="row4-6c">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="row4-6c">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="row4-6c">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="row4-6c">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="row4-6c">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="row4-6c">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="row4-6c">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="row4-6c">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="row4-6c">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="row4-6c">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div className="row5">
+          <div className="sellos sellosTitle">
+            <b>Sello de Unidad</b>
+            <b>Nombre y firma del Capacitador</b>
+          </div>
+          <div className="sellos">
+            <span></span>
+            <span className="fename"> Ing. Ricardo Del Rio </span>
+          </div>
+        </div>
+        <div className="row6">
+          <b className="footer">
+            Nuestro compromiso es su satisfacción total. Si necesita contactar a
+            GE Healthcare y GE Sistemas Medicos de México S.A. de C.V. por
+            cualquier otro motivo que no sea una llamada de servicio y los
+            canales normales de comunicación no le contestan satisfactoriamente,
+            entre en el siguiente sitio web: http://ecso.gehealthcare.com
+          </b>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Capacitacion;
